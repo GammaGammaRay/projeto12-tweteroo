b1c7dab136f478057a47f7b7db814a07d9317ca5
GammaGammaRay <benjamin.peret@gmail.com>
2023-06-26 14:40:35 -0300

    /sign-up and /tweets POST

--- src/app.js
+++ src/app.js
@@ -1,40 +1,57 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const PORT = 5000;
 
-const users = []
-const tweets = []
+const users = [];
+const tweets = [];
 
 app.post("/sign-in", (req, res) => {
-    const { username, avatar } = req.body
+    const { username, avatar } = req.body;
 
-    if(!username || !avatar || typeof username !== 'string' || typeof avatar !== 'string' ) {
-        return res.statusCode(400).send("All fields are required")
-    }
-
-    users.push({username, avatar})
-    return res.status(201).send("OK")
-})
-
-app.post("/tweets", (req, res) => {
-    const username = req.headers.user
-    const { tweet } = req.body
-
-    if(!users.find((user) => user.username === username)) {
-        return res.status(401).send("Unauthorized attempt")
-    }
-    if (!tweet) {
+    if (!username || !avatar || typeof username !== 'string' || typeof avatar !== 'string') {
         return res.status(400).send("All fields are required");
     }
-    tweets.push({username, tweet})
-    res.status(201).send("Ok")
-})
 
+    users.push({ username, avatar });
+    return res.status(201).send("OK");
+});
 
+app.post("/tweets", (req, res) => {
+    const username = req.headers.user;
+    const { tweet } = req.body;
 
-app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+    if (!users.find((user) => user.username === username)) {
+        return res.status(401).send("UNAUTHORIZED");
+    }
+    if (!tweet || typeof tweet !== 'string') {
+        return res.status(400).send("All fields are required");
+    }
+    tweets.push({ username, tweet });
+    res.status(201).send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+    let page = parseInt(req.query.page, 10) || 1;
+
+    if (page < 1 || isNaN(page)) {
+        return res.status(400).send("Page is not valid");
+    }
+
+    const startIndex = (page - 1) * 10;
+    const endIndex = startIndex + 10;
+
+    const indexedTweets = tweets.slice(startIndex, endIndex).map((tweet) => ({
+        username: tweet.username,
+        avatar: users.find((user) => user.username === tweet.username).avatar,
+        tweet: tweet.tweet,
+    }));
+
+    res.send(indexedTweets);
+});
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
