0aa7293d68c13ed5a8be07410c9e8f65605f874b
GammaGammaRay <benjamin.peret@gmail.com>
2023-06-26 14:32:27 -0300

    /sign-up and /tweets POST

--- src/app.js
+++ src/app.js
@@ -1,9 +1,40 @@
-import express from 'express';
+import express from 'express'
 import cors from 'cors'
-import nodemon from 'nodemon';
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = 5000;
+
+const users = []
+const tweets = []
+
+app.post("/sign-in", (req, res) => {
+    const { username, avatar } = req.body
+
+    if(!username || !avatar || typeof username !== 'string' || typeof avatar !== 'string' ) {
+        return res.statusCode(400).send("All fields are required")
+    }
+
+    users.push({username, avatar})
+    return res.status(201).send("OK")
+})
+
+app.post("/tweets", (req, res) => {
+    const username = req.headers.user
+    const { tweet } = req.body
+
+    if(!users.find((user) => user.username === username)) {
+        return res.status(401).send("Unauthorized attempt")
+    }
+    if (!tweet) {
+        return res.status(400).send("All fields are required");
+    }
+    tweets.push({username, tweet})
+    res.status(201).send("Ok")
+})
 
 
-app.listen(4000);
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`))
